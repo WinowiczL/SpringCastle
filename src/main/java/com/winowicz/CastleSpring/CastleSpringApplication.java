af43078fb0244ab2bd2b37059375502407ae56e9
Lukasz Winowicz <winowiczlukasz@gmail.com>
2019-01-11 22:15:21 +0100

    Init

---
+++ src/main/java/com/winowicz/CastleSpring/CastleSpringApplication.java
@@ -0,0 +1,14 @@
+package com.winowicz.CastleSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastleSpringApplication.class, args);
+	}
+
+}
+
